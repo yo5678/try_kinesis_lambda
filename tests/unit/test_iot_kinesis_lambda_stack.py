bef8e7a5b0ab7b95f8061cfaceca62f9254410bf
yo5678 <yosuke.i.5678@gmail.com>
2023-10-01 13:34:20 +0900

    勉強がてら参考URLのコピー

--- tests/unit/test_iot_kinesis_lambda_stack.py
+++ tests/unit/test_iot_kinesis_lambda_stack.py
@@ -1,7 +1,8 @@
 import aws_cdk as core
 import aws_cdk.assertions as assertions
 
-from iot_kinesis_lambda.iot_kinesis_lambda_stack import IotKinesisLambdaStack
+from iot_kinesis_lambda_cdk.iot_kinesis_lambda_cdk_stack import IotKinesisLambdaStack
+
 
 # example tests. To run these tests, uncomment this file along with the example
 # resource in iot_kinesis_lambda/iot_kinesis_lambda_stack.py
@@ -9,6 +10,7 @@ def test_sqs_queue_created():
     app = core.App()
     stack = IotKinesisLambdaStack(app, "iot-kinesis-lambda")
     template = assertions.Template.from_stack(stack)
+
 
 #     template.has_resource_properties("AWS::SQS::Queue", {
 #         "VisibilityTimeout": 300
